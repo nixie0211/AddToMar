028e6c10b61b9665cb43cddab501be26bd275867
nix <nixie0211@users.noreply.github.com>
2026-09-17 12:38:20 +0800

    Make pharmacy order item notes red so they stand out from other details.

--- pharmacy/index.php
+++ pharmacy/index.php
@@ -33,7 +33,7 @@ require_once dirname(__DIR__) . '/includes/maps.php';
 <link rel="stylesheet" href="<?= htmlspecialchars(app_url('css/map-service-overlay.css'), ENT_QUOTES, 'UTF-8') ?>?v=street-map-2">
 <link rel="stylesheet" href="css/base.css?v=unified-mint-outer-canvas">
 <link rel="stylesheet" href="<?= htmlspecialchars(app_url('css/rx-crop-preview.css'), ENT_QUOTES, 'UTF-8') ?>?v=rx-crop-pan-1">
-<link rel="stylesheet" href="css/orders.css?v=order-progress-4">
+<link rel="stylesheet" href="css/orders.css?v=order-note-red-1">
 <link rel="stylesheet" href="css/reports.css">
 <link rel="stylesheet" href="css/settings.css?v=profile-avatar-mint">
 <link rel="stylesheet" href="css/add-medicine.css?v=saving-overlay-1">
