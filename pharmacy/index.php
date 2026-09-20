1cdd749743e53e81819241caba73e2c128438d96
nix <nixie0211@users.noreply.github.com>
2026-09-21 00:22:31 +0800

    Remove the outer card around pharmacy order details.

--- pharmacy/index.php
+++ pharmacy/index.php
@@ -32,7 +32,7 @@ require_once dirname(__DIR__) . '/includes/maps.php';
 <link rel="stylesheet" href="<?= htmlspecialchars(app_url('css/map-service-overlay.css'), ENT_QUOTES, 'UTF-8') ?>?v=street-map-2">
 <link rel="stylesheet" href="css/base.css?v=pharmacy-nav-badge-1">
 <link rel="stylesheet" href="<?= htmlspecialchars(app_url('css/rx-crop-preview.css'), ENT_QUOTES, 'UTF-8') ?>?v=rx-crop-contain-1">
-<link rel="stylesheet" href="css/orders.css?v=pharmacy-order-panel-1">
+<link rel="stylesheet" href="css/orders.css?v=pharmacy-order-panel-2">
 <link rel="stylesheet" href="css/reports.css">
 <link rel="stylesheet" href="css/settings.css?v=settings-save-center-1">
 <link rel="stylesheet" href="css/add-medicine.css?v=saving-overlay-1">
