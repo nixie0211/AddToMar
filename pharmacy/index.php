2b82c8a29dbf4cff9d1999c5416d3f94f270bb95
nix <nixie0211@users.noreply.github.com>
2026-09-21 10:30:59 +0800

    Restore the pharmacy logout button to its original left alignment.

--- pharmacy/index.php
+++ pharmacy/index.php
@@ -30,7 +30,7 @@ require_once dirname(__DIR__) . '/includes/maps.php';
 <?php include PHARMACY_ROOT . '/partials/head.php'; ?>
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="">
 <link rel="stylesheet" href="<?= htmlspecialchars(app_url('css/map-service-overlay.css'), ENT_QUOTES, 'UTF-8') ?>?v=street-map-2">
-<link rel="stylesheet" href="css/base.css?v=pharmacy-logout-center-1">
+<link rel="stylesheet" href="css/base.css?v=pharmacy-logout-left-1">
 <link rel="stylesheet" href="<?= htmlspecialchars(app_url('css/rx-crop-preview.css'), ENT_QUOTES, 'UTF-8') ?>?v=rx-crop-contain-1">
 <link rel="stylesheet" href="css/orders.css?v=pharmacy-status-colors-1">
 <link rel="stylesheet" href="css/reports.css">
